01555ec55761549350bc2050815c6f99b2945a7f
Icorrea7 <isacorrea07@gmail.com>
2018-04-25 18:57:41 -0500

    api product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products #los pasamos a json para que nos retorne todos nuestros articles
+    @products = Product.all
+    render json: @products
   end
 end
